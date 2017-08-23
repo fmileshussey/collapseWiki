25e94c182757614ce03c7f18888159955e859b70
Finnegan Miles-Hussey <fmileshussey@outlook.com>
2017-08-23 23:06:03 +1200

    Got divs succesfully being made between h2's

--- src/collapse.js
+++ src/collapse.js
@@ -1,27 +1,38 @@
-createCollapsables();
+/**
+	collapse.js - firefox addon for collapsing wikipedia
+	article sections.
+	@author fmileshussey
+	@version 0.1.0
+*/
 
+window.onload = createCollapsables;
+
+/**
+	createCollapsable - encloses tags between h2's in a div
+	that are used to collapse sections.
+*/
 function createCollapsables(){
     var headers = document.querySelectorAll("h2");
-    var arr = [];
+	var arr = [];
+	
+	Array.from(headers).forEach(function(h2, index) {
+		arr[index] = [];
+		var curr = h2.nextElementSibling;
+		
+		while (curr.tagName !== "H2") {
+			arr[index].push(curr);
+			var next = curr.nextElementSibling;
+			curr = next;
+			if(curr == null) break;
+		}
+	});
 
-    Array.from(headers).forEach(function(h2, index) {
-	arr[index] = [];
-	var current = h2.nextElementSibling;
-	while (current.tagName !== "h2"){
-	    arr[index].push(curr);
-	    var next = curr.nextElementSibling;
-	    current = next;
-	}
-    });
-
-    arr.forEach(function(html, index){
-	var div = document.createElement("div");
-	div.className = "collapseSect";
-	document.body.appendChild(div);
-	//div.appendChild(headers[index]);
-	html.forEach(function(el){
-	    div.appendChild(el);
-	})
-    })
+	arr.forEach(function(html, index) {
+		document.body.appendChild(headers[index])
+		var div = document.createElement("div");
+		document.body.appendChild(div);
+		html.forEach(function(el) {
+			div.appendChild(el)
+		})
+	});
 }
-
