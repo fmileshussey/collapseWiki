c23d26c2b075575e1124edaaf18631286c6231be
Finnegan Miles-Hussey <fmileshussey@outlook.com>
2017-08-28 21:14:40 +1200

    fixed the navigation bars accidentally moving out of place, added id's to each div

--- src/collapse.js
+++ src/collapse.js
@@ -2,7 +2,7 @@
 	collapse.js - firefox addon for collapsing wikipedia
 	article sections.
 	@author fmileshussey
-	@version 0.1.1
+	@version 0.1.2
 */
 
 window.onload = createCollapsables;
@@ -16,24 +16,26 @@ function createCollapsables(){
 	var insLoc = document.getElementById("bodyContent");
 	var finalItem = document.getElementById("catlinks");
 	
-	var arr = [];
+	var array = [];
 	
 	Array.from(headers).forEach(function(h2, index) {
-		arr[index] = [];
-		var curr = h2.nextElementSibling;
+		array[index] = [];
+		var current = h2.nextElementSibling;
 		
-		while (curr.tagName !== "H2") {
-			arr[index].push(curr);
-			var next = curr.nextElementSibling;
-			curr = next;
-			if(curr == null) break;
+		while (current.tagName !== "H2") {
+			array[index].push(current);
+			var next = current.nextElementSibling;
+			current = next;
+			if(current == null) break;
 		}
 	});
 
-	arr.forEach(function(html, index) {
-		if(headers[index].innerHTML != "Contents"){
+	array.forEach(function(html, index) {
+		if(headers[index].innerHTML != "Contents" && headers[index].innerHTML != "Navigation menu"){
 			insLoc.insertBefore(headers[index], finalItem);
 			var div = document.createElement("div");
+			div.id = index + "-content";
+			console.log(headers[index].nextElementSibling.tagName);
 			insLoc.insertBefore(div, finalItem);
 			html.forEach(function(el) {
 				div.appendChild(el)
