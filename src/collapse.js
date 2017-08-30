55192e463a38ae95f2cf3167ae85e936f4a4d0f7
Finnegan Miles-Hussey <fmileshussey@outlook.com>
2017-08-30 18:38:58 +1200

    got core component working correctly, sections are displaying on click

--- src/collapse.js
+++ src/collapse.js
@@ -2,10 +2,19 @@
 	collapse.js - firefox addon for collapsing wikipedia
 	article sections.
 	@author fmileshussey
-	@version 0.1.2
+	@version 0.2.0
 */
 
 window.onload = createCollapsables;
+
+function hideDiv(index){
+	var d = document.getElementById(index + "-content");
+	if(d.style.display == "none"){
+		d.style.display = "block";
+	}else{
+		d.style.display = "none";
+	}
+}
 
 /**
 	createCollapsable - encloses tags between h2's in a div
@@ -32,10 +41,14 @@ function createCollapsables(){
 
 	array.forEach(function(html, index) {
 		if(headers[index].innerHTML != "Contents" && headers[index].innerHTML != "Navigation menu"){
+			var hideLink = document.createElement("span");
+			hideLink.classList.add("mw-editsection");
+			hideLink.innerHTML = "[<a onclick='hideDiv(" + index + ")'>hide</a>]";
+			headers[index].appendChild(hideLink);
 			insLoc.insertBefore(headers[index], finalItem);
 			var div = document.createElement("div");
 			div.id = index + "-content";
-			console.log(headers[index].nextElementSibling.tagName);
+			//console.log(headers[index].nextElementSibling.tagName);
 			insLoc.insertBefore(div, finalItem);
 			html.forEach(function(el) {
 				div.appendChild(el)
