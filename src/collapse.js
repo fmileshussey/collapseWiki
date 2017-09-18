27f9b1f58dc66cd1aee32435d5b946efd9a53aa7
elouia <fmileshussey@outlook.com>
2017-09-18 18:58:03 +1200

    Commented code and fixed to work on articles in different languages

--- src/collapse.js
+++ src/collapse.js
@@ -1,68 +1,84 @@
 /**
-	collapse.js - firefox addon for collapsing wikipedia
-	article sections.
-	@author fmileshussey
-	@version 0.3.0
+   collapse.js
+   @desc firefox addon for collapsing wikipedia
+   article sections.
+   @author fmileshussey
+   @version 0.3.0
 */
 
 window.onload = createCollapsables();
 
+
+/**
+   @desc the function called on event i.e. click, hides
+   and displays divs depending on current state and updates
+   link.
+   @param index - index of current section to be changed
+
+ */
 function hideDiv(index){
-	var d = document.getElementById(index + "-content");
-	var heading = document.getElementById(index + "-link");
-	if(d.style.display == "none"){
-		d.style.display = "block";
-		heading.innerHTML = "hide";
-	}else{
-		d.style.display = "none";
-		heading.innerHTML = "show";
-	}
+    var d = document.getElementById(index + "-content");
+    var heading = document.getElementById(index + "-link");
+    if(d.style.display == "none"){
+	d.style.display = "block";
+	heading.innerHTML = "hide";
+    }else{
+	d.style.display = "none";
+	heading.innerHTML = "show";
+    }
 }
 
 /**
-	createCollapsable - encloses tags between h2's in a div
-	that are used to collapse sections.
+   @desc createCollapsable - encloses tags between h2's in a div
+   that are used to collapse sections.
 */
 function createCollapsables(){
     var headers = document.querySelectorAll("h2");
-	var insLoc = document.getElementById("bodyContent");
-	var finalItem = document.getElementById("catlinks");
-	var array = [];
+    var insLoc = document.getElementById("bodyContent");
+    var finalItem = document.getElementById("catlinks");
+    var array = [];
+
+
+    /*For each header's siblings, add into array */
+    Array.from(headers).forEach(function(h2, index) {
+	array[index] = [];
+	var current = h2.nextElementSibling;
 	
-	Array.from(headers).forEach(function(h2, index) {
-		array[index] = [];
-		var current = h2.nextElementSibling;
+	while (current.tagName !== "H2") {
+	    array[index].push(current);
+	    var next = current.nextElementSibling;
+	    current = next;
+	    if(current == null) break;
+	}
+    });
+
+    /*Add links, create and add elements to individual divs*/
+    array.forEach(function(html, index) {
+	
+	if(headers[index].innerHTML != "Contents"){
+	    if(headers[index].parentElement.id != "mw-navigation"){
+		//headers[index].innerHTML != "Navigation menu"
+		//parentElement.id == mw-navigation
 		
-		while (current.tagName !== "H2") {
-			array[index].push(current);
-			var next = current.nextElementSibling;
-			current = next;
-			if(current == null) break;
-		}
-	});
-	
-	array.forEach(function(html, index) {
+		//inserting link
+		var hideLink = document.createElement("span");
+		hideLink.style.marginRight = "5px";
+		hideLink.style.fontSize = "small";
+		hideLink.style.fontFamily = "sans-serif";
+		hideLink.innerHTML = "[<a id='" + index + "-link'>hide</a>]";
+		headers[index].insertBefore(hideLink, headers[index].firstChild);
 		
-		if(headers[index].innerHTML != "Contents" && headers[index].innerHTML != "Navigation menu"){
-			
-			//inserting link
-			var hideLink = document.createElement("span");
-			hideLink.style.marginRight = "5px";
-			hideLink.style.fontSize = "small";
-			hideLink.style.fontFamily = "sans-serif";
-			hideLink.innerHTML = "[<a id='" + index + "-link'>hide</a>]";
-			headers[index].insertBefore(hideLink, headers[index].firstChild);
-			
-			insLoc.insertBefore(headers[index], finalItem);
-			var div = document.createElement("div");
-			div.id = index + "-content";
-			
-			insLoc.insertBefore(div, finalItem);
-			html.forEach(function(el) {
-				div.appendChild(el)
-			})
-			document.getElementById(index + "-link").addEventListener("click", function(){hideDiv(index)});
-		}
-	});
-	
+		insLoc.insertBefore(headers[index], finalItem);
+		var div = document.createElement("div");
+		div.id = index + "-content";
+		
+		insLoc.insertBefore(div, finalItem);
+		html.forEach(function(el) {
+		    div.appendChild(el)
+		})
+		document.getElementById(index + "-link").addEventListener("click", function(){hideDiv(index)});
+	    }
+	}
+    });
+    
 }
